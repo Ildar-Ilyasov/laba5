9fed21ce8c57e8733e59647f0248345cbeefa071
Ildar-Ilyasov <ildar.ilyasov123@yandex.com>
2023-02-25 01:08:01 +0300

    Initial commit

---
+++ src/laba5.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class laba5 {
+    public static void main (String[] args) {
+        Scanner in = new Scanner (System.in );
+        int[] mass = new int[in.nextInt()];
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = in.nextInt();
+        }
+        vuv (mass);
+        alg (mass);
+        System.out.println();
+        vuv(mass);
+    }
+
+    static void vuv(int[] mass) {
+        for (int i = 0; i < mass.length; i++) {
+            System.out.print(mass[i] + " ");
+        }
+    }
+
+    static void alg(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minId = i;
+            for (int j = i+1; j < array.length; j++) {
+                if (array [j] < min) {
+                    min = array [j];
+                    minId = j;
+                }
+            }
+            int temp = array[i];
+            array [i] = min;
+            array[minId] = temp;
+        }
+    }
+}
